b81463a758532a88e2da84debdd83c81e264a158
67219100082-byte <67219100082@ptl.ac.th>
2026-07-13 12:58:50 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEwmfoYa_VmnOQ9ueyZ5Tk8CJVmxDEV2w",
+  authDomain: "petpawth2026.firebaseapp.com",
+  projectId: "petpawth2026",
+  storageBucket: "petpawth2026.firebasestorage.app",
+  messagingSenderId: "585605372616",
+  appId: "1:585605372616:web:a36fdca28268414456e3fd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+export { auth, db, storage };
