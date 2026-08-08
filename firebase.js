70ae97da4a7f4fa73547aa86ff4c2592c990d247
67219100082-byte <67219100082@ptl.ac.th>
2026-08-08 16:43:33 +0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -5,25 +5,42 @@ import {
     setPersistence,
     browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEwmfoYa_VmnOQ9ueyZ5Tk8CJVmxDEV2w",
-  authDomain: "petpawth2026.firebaseapp.com",
-  projectId: "petpawth2026",
-  storageBucket: "petpawth2026.firebasestorage.app",
-  messagingSenderId: "585605372616",
-  appId: "1:585605372616:web:a36fdca28268414456e3fd"
+    apiKey: "AIzaSyAEwmfoYa_VmnOQ9ueyZ5Tk8CJVmxDEV2w",
+    authDomain: "petpawth2026.firebaseapp.com",
+    projectId: "petpawth2026",
+    storageBucket: "petpawth2026.firebasestorage.app",
+    messagingSenderId: "585605372616",
+    appId: "1:585605372616:web:a36fdca28268414456e3fd"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-await setPersistence(auth, browserLocalPersistence);
+await setPersistence(
+    auth,
+    browserLocalPersistence
+);
 
 const db = getFirestore(app);
+
 const storage = getStorage(app);
 
-export { auth, db, storage };
+
+export {
+    auth,
+    db,
+    storage
+};
